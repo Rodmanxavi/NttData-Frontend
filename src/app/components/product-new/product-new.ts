e32f6270b34441bc1c5b364565ad85f4e377579a
Rodmanxavi <rodman.iniguez@epn.edu.ec>
2025-07-26 18:53:02 -0500

    Creación proyecto y componentes

---
+++ src/app/components/product-new/product-new.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-new',
+  imports: [],
+  templateUrl: './product-new.html',
+  styleUrl: './product-new.css'
+})
+export class ProductNew {
+
+}
